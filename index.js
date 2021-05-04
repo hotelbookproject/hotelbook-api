130d7add0fd107c3677c6382ad061f3b89555d08
Nithin K Joy <62066971+nithinkjoy-tech@users.noreply.github.com>
2021-05-04 19:16:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ require("dotenv").config();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
- 
+ //removed comments
 const port = process.env.PORT || 3800;
 app.listen(port, () => console.log(`Listening to port ${port}`));
